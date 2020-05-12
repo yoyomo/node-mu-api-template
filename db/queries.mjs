7e25c34437e7cda9bbebe28ac15f38f421d962e1
Armando <armando.josue.og@gmail.com>
2020-05-11 23:58:22 -0700

    get id

--- db/queries.mjs
+++ db/queries.mjs
@@ -7,61 +7,56 @@ export default {
 
     const tables = { users: true }
 
-    return {
-      GET: async table => {
-        if (!tables[table]) throw "Table does not exist"
-        const results = await db.query('SELECT * FROM ' + table + ' ORDER BY id ASC')
-        return { status: 200, data: results.rows };
-      },
-
-      getUserById: (request, response) => {
-        const id = parseInt(request.params.id)
-
-        db.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
-          if (error) {
-            throw error
+    return table => {
+      if (!tables[table]) throw "Table does not exist"
+      return {
+        GET: async (id) => {
+          let results = { rows: [] };
+          if (id) {
+            results = await db.query('SELECT * FROM "' + table + '" WHERE id = $1 LIMIT 1', [id])
+          } else {
+            results = await db.query('SELECT * FROM "' + table + '" ORDER BY id ASC')
           }
-          response.status(200).json(results.rows)
-        })
-      },
+          return { status: 200, data: results.rows };
+        },
+        createUser: (request, response) => {
+          const { name, email } = request.body
 
-      createUser: (request, response) => {
-        const { name, email } = request.body
-
-        db.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
-          if (error) {
-            throw error
-          }
-          response.status(201).send(`User added with ID: ${result.insertId}`)
-        })
-      },
-
-      updateUser: (request, response) => {
-        const id = parseInt(request.params.id)
-        const { name, email } = request.body
-
-        db.query(
-          'UPDATE users SET name = $1, email = $2 WHERE id = $3',
-          [name, email, id],
-          (error, results) => {
+          db.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email], (error, results) => {
             if (error) {
               throw error
             }
-            response.status(200).send(`User modified with ID: ${id}`)
-          }
-        )
-      },
+            response.status(201).send(`User added with ID: ${result.insertId}`)
+          })
+        },
 
-      deleteUser: (request, response) => {
-        const id = parseInt(request.params.id)
+        updateUser: (request, response) => {
+          const id = parseInt(request.params.id)
+          const { name, email } = request.body
 
-        db.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
-          if (error) {
-            throw error
-          }
-          response.status(200).send(`User deleted with ID: ${id}`)
-        })
-      },
+          db.query(
+            'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+            [name, email, id],
+            (error, results) => {
+              if (error) {
+                throw error
+              }
+              response.status(200).send(`User modified with ID: ${id}`)
+            }
+          )
+        },
+
+        deleteUser: (request, response) => {
+          const id = parseInt(request.params.id)
+
+          db.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+            if (error) {
+              throw error
+            }
+            response.status(200).send(`User deleted with ID: ${id}`)
+          })
+        },
+      }
     }
   }
 }
--- index.mjs
+++ index.mjs
@@ -1,27 +1,29 @@
 import http from 'http';
+import URL  from 'url';
+
 import db from './db/queries.mjs';
 import colors from './utils/colors.mjs';
 
 const PORT = process.env.PORT || 1234;
 
-db.connect();
+const query = db.connect();
 
 http.createServer(async (req, res) => {
 
-  const url = req.url;
+  const url = URL.parse(req.url, true);
   const method = req.method;
 
   let response = { status: 404, error: "Page not found" };
 
   try {
-    if (url === '/users') {
-      response = await db[method]('users');
+    if (url.pathname === '/users') {
+      response = await query('users')[method](url.query.id);
     }
   } catch (error) {
-    response.error = error;
+    response.error = error.message;
   }
 
-  console.log(`${colors.cyan}%s${colors.reset} %s %s %O`, new Date(), method, url, response);
+  console.log(`${colors.cyan}%s${colors.reset} %s %s %O`, new Date(), method, url.path, response);
 
   res.writeHead(response.status, { 'Content-Type': 'application/json' });
 
